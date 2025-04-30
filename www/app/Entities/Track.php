1849223bf2edbe7c0799092d3794b334e11ad6b2
Marta García González <marta.ggonzalez@salle.url.edu>
2025-04-30 17:48:39 +0200

    BaseView.php -> template view created and can be used to generate the other views. Track.php -> class created and constructor set, functions will be added as needed logoTemp.png -> temp image of the logo

---
+++ www/app/Entities/Track.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Entities;
+class Track
+{
+    private $id;
+    private $name;
+    private $cover;
+    private $artist;
+    private $artistID;
+    private $album;
+    private $albumID;
+    private $duration;
+    private $playerURL;
+
+
+    public function __construct($id, $name, $cover, $artist, $artistID, $album, $albumID, $duration, $playerURL)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->cover = $cover;
+        $this->artist = $artist;
+        $this->artistID = $artistID;
+        $this->album = $album;
+        $this->albumID = $albumID;
+        $this->duration = $duration;
+        $this->playerURL = $playerURL;
+
+    }
+
+}
---
+++ www/app/Views/BaseView.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $this->renderSection('title') ?></title>
+    <?= $this->renderSection('css') ?>
+    <style>
+        body{
+            margin: 0;
+        }
+        #content{
+            margin: 10px;
+            margin-top: 134px;
+        }
+        #logo{
+            height: 70px;
+            cursor: pointer;
+            margin: 20px;
+            border-radius: 30%;
+        }
+        #right {
+            margin-right: 40px;
+        }
+        header {
+            background-color: #ffcdb2;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 10px;
+            position: fixed;
+            top: 0;
+            width: 100%;
+        }
+
+    </style>
+</head>
+    <body>
+        <header>
+            <a href="<?=base_url('/home');?>"><img src="<?=base_url('/logoTemp.png');?>" alt="Logo" id="logo"></a>
+            <!--center nav: para poner algo en el centro de la barra de nav (nada / seach bar)-->
+            <div><?=$this->renderSection('centerNav')?></div>
+            <!--side nav: para poner algo en el lado de la barra de nav ex (botones log in / boton myplaylists, user, log out /+ boton main menu etc)-->
+            <div id="right"><?=$this->renderSection('sideNav')?></div>
+        </header>
+        <div id="content">
+            <?= $this->renderSection('content') ?>
+        </div>
+    </body>
+</html>
