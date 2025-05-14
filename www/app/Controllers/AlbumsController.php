e5baeca09ac872cab3dff0612fb631cd2a193cf0
Marta haro <marta.haro@students.salle.url.edu>
2025-05-14 16:48:11 +0200

    The album page is started. If an album is searched we go there. shows -Image -Artist -Title -Date Is prepared to show the douration

--- www/app/Controllers/AlbumsController.php
+++ www/app/Controllers/AlbumsController.php
@@ -1,11 +1,33 @@
 <?php
 
 namespace App\Controllers;
+use GuzzleHttp\Client;
 
 class AlbumsController extends BaseController
 {
-    public function show($albumId){
-        return "Album Id is $albumId";
-    }
+    public function show($albumId)
+    {
+        helper('form');
+        $client   = new Client();
 
+        $albumUrl = "https://api.jamendo.com/v3.0/albums/?client_id=9d42fee4&id=$albumId";
+        $response = $client->request('GET', $albumUrl);
+        $body     = json_decode($response->getBody(), true);
+
+        if (empty($body['results']) || !isset($body['results'][0])) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException("Album not found");
+        }
+
+        $album = $body['results'][0];
+
+        $albumData = [
+            'album_Name'               => $album['name'],
+            'artist_Name'              => $album['artist_name'],
+            'album_Cover'              => $album['image'],
+            'album_ReleaseDate'        => $album['releasedate'],
+            'album_DisplayReleaseDate' => date('F j, Y', strtotime($album['releasedate'])),
+        ];
+
+        return view('AlbumPage', $albumData);
+    }
 }
---
+++ www/app/Views/AlbumPage.php
@@ -0,0 +1,43 @@
+<!--Extending from BaseView contains the general elements of the file. For avoid repeating common elements trough pages-->
+<?= $this->extend('LogBaseView') ?>
+
+<!--Title of the page will appear on the navbar-->
+<?= $this->section('title') ?>
+Album | LSpoty
+<?= $this->endSection() ?>
+
+<!--We link the CSS to format the page-->
+<?= $this->section('CSS') ?>
+<link rel="stylesheet" href="/CSS/AlbumView_styles.css">
+<?= $this->endSection() ?>
+
+
+<?= $this->section('content') ?>
+<!--Main where all the content of the page will be stored-->
+<main class="album-main">
+    <!--Article which contains the general info of the album-->
+    <article aria-labelledby="album-title">
+        <!--Section in which will appear the image of the album, the title and artist name-->
+        <section class="album-banner">
+            <figure class="album-image" role="img" aria-label="Album cover">
+                <img src="<?= esc($album_Cover) ?>" alt="Album cover">
+            </figure>
+            <div class="album-information-I">
+                <h1 id="album-title" class="album-title"><?= esc($album_Name) ?></h1>
+                <p class="artist-name"><?= esc($artist_Name) ?></p>
+            </div>
+        </section>
+        <!--Section in which will appear the date of release and the duration of the album-->
+        <section class="album-information-II">
+            <p>
+                Released: <time datetime="<?= esc($album_ReleaseDate) ?>"><?= esc($album_DisplayReleaseDate) ?></time>
+                &nbsp;|&nbsp;
+                Duration: <time datetime="<?= esc($album_ReleaseDate) ?>"><?= esc($album_DisplayReleaseDate) ?></time>
+            </p>
+        </section>
+
+
+    </article>
+</main>
+
+<?= $this->endSection() ?>
